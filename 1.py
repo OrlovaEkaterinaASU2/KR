6c30d76efc369197b8507b686fa5101a04778285
polich7 <phominykh1997@gmail.com>
2019-10-19 14:55:29 +0500

    добавлены ключевые слова передвижения

--- 1.py
+++ 1.py
@@ -17,3 +17,15 @@ for word in ls1:
         is_name = any('Name' in p.tag for p in morph.parse(word))
         if (is_name):
             print(word)
+with open('2.txt') as movement:  # приводим передвижения в нормальную форму
+    ls2 = []
+    for line in movement:
+        lst2 = line.split()
+        words2 = []
+        for word in lst2:
+            p = morph.parse(word)[0]  # делаем разбор
+            words2.append(p.normal_form)
+        ls2.append(words2)
+for i in ls2:
+    move = i
+print(move)
