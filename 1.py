d3bd0a6577c81b802fc119c9b3595b2687f7992e
OrlovaEkaterinaASU2 <orlova_ekaterina_97@mail.ru>
2019-10-22 09:46:23 +0500

    Теперь умеет выделять города

--- 1.py
+++ 1.py
@@ -1,5 +1,8 @@
 import pymorphy2
+import natasha
+from collections import OrderedDict
 morph = pymorphy2.MorphAnalyzer()
+ls1=list()
 with open('1.txt') as f:
     ls = []
     for line in f:
@@ -29,3 +32,21 @@ with open('2.txt') as movement:  # приводим передвижения в 
 for i in ls2:
     move = i
 print(move)
+lines = [
+    'тамань', 'пермь','капуста','В Чеченской республике на день рождения ...',
+    'Донецкая народная республика провозгласила ...',
+    'Российская Федерация в Соединенных Штатах Америки',
+    'речь шла о Обьединенных Арабских Эмиратах Соединённые Штаты'
+]
+extractor = natasha.LocationExtractor()
+#for line in ls1:
+for line in ls1:
+    try:
+        matches = extractor(line)
+        match = matches[0]
+        fact = match.fact.as_json
+        #print(line)
+        print(dict(fact)['name'])
+    except:
+        print(end='')
+
