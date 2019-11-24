332e19c677ed1fb0730d39237013301a959891a7
OrlovaEkaterinaASU2 <orlova_ekaterina_97@mail.ru>
2019-11-24 19:42:05 +0500

    отображает на карте перемещение Фогга, не хватает только стрелочек

--- 1.py
+++ 1.py
@@ -1,81 +1,65 @@
 import pymorphy2
+import geocoder
 import natasha
 from collections import OrderedDict
-import geocoder
-#блок для поиски имен
 morph = pymorphy2.MorphAnalyzer()
-ls1=list()
-with open('1.txt') as f:
-    ls = []
+#_________передвижение
+with open('2.txt') as movement: # ключевые слова передвижения
+    lst2 = []
+    for line in movement:
+        lst2 = line.split()
+print(lst2)
+phileasfogg = {}
+k=0
+with open('Верн Жюль. Вокруг света в восемьдесят дней - royallib.com.txt') as f:
+    ls = list() # запишем весь текст разбитый на слова
     for line in f:
         lst = line.split()
-        words = []
+        words = [] # слова из одной строки
         for word in lst:
             p = morph.parse(word)[0]  # делаем разбор
             words.append(p.normal_form)
-        ls.append(words)
-for i in ls:
-    ls1 = i
-#print(ls1)
-for word in ls1:
-    for p in morph.parse(word):
-        is_name = any('Name' in p.tag for p in morph.parse(word))
-        exc=['том','из','арен','герой','гора','брюнна','слава','«право']
-        if (is_name):
-            if (word not in exc):
-                print(word)
-with open('2.txt') as movement:  # приводим передвижения в нормальную форму
-    ls2 = []
-    for line in movement:
-        lst2 = line.split()
-        words2 = []
-        for word in lst2:
-            p = morph.parse(word)[0]  # делаем разбор
-            words2.append(p.normal_form)
-        ls2.append(words2)
-for i in ls2:
-    move = i
-#print(move)
-# блок для поиска городов
-lines = [
-    'тамань', 'пермь','капуста','В Чеченской республике на день рождения ...',
-    'Донецкая народная республика провозгласила ...',
-    'Российская Федерация в Соединенных Штатах Америки',
-    'речь шла о Обьединенных Арабских Эмиратах Соединённые Штаты'
-]
-extractor = natasha.LocationExtractor()
-for line in ls1:
-    try:
-        matches = extractor(line)
-        match = matches[0]
-        fact = match.fact.as_json
-        exc1 = ['ростов', 'уста', 'мирный', 'главнокомандовавший', 'билибино', 'тушино','пара','билибин']
-        if (dict(fact)['name'] not in exc1):
-            print(dict(fact)['name'])
-    except:
-        print(end='')
-# находим координаты
+        ls += words
+for i in range(len(ls)-1):
+    for move in lst2:
+        if ls[i] == move:
+            ls1 = ls[i-25:i+25]
+            p1 = False
+            p2 = False
+            for j in range(len(ls1)-1):
+                p = morph.parse(ls1[j])[0]
+                if ('Name' in p.tag) and ((p.normal_form == 'филеасу') or (p.normal_form == 'фогга')):
+                    p1 = True
+                elif ('Geox' in p.tag):
+                    p2 = True
+                    g = [ls1[j]]
+            if p1 == True and p2 == True:
+                phileasfogg[k] = g
+                k += 1
+location1= []
 from geopy.geocoders import Nominatim
-geolocator = Nominatim(user_agent="specify_your_app_name_here")
-location = geolocator.geocode("Москва")
-print((location.latitude, location.longitude))
-loc1 = [location.latitude, location.longitude]
-location = geolocator.geocode("Париж") # типа франция
-print((location.latitude, location.longitude))
-loc2 = [location.latitude, location.longitude]
-location = geolocator.geocode("Санкт-Петербург")
-print((location.latitude, location.longitude))
-loc3 = [location.latitude, location.longitude]
-location = geolocator.geocode("Вена")  # типа австрия
-print((location.latitude, location.longitude))
-loc4 = [location.latitude, location.longitude]
-location = geolocator.geocode("Браунау-ам-Инн")  # типа австрия
-print((location.latitude, location.longitude))
-loc5 = [location.latitude, location.longitude]
-# отображение на карте
+for key in phileasfogg.keys():
+    if key != 0 or key != 1:
+        try:
+            phileasfogg[key]
+            geolocator = Nominatim(user_agent="specify_your_app_name_here")
+            location = geolocator.geocode(phileasfogg[key])
+            location1 += [list([location.latitude, location.longitude])]
+            phileasfogg[key].append([location.latitude, location.longitude])
+        except:
+            phileasfogg[key] = ['no']
+    else:
+        phileasfogg[key] = ['No']
+for key in phileasfogg.keys():
+    print(phileasfogg[key])
+location1.insert(0,[51.5073219, -0.1276474]) # координаты лондона
+location1.append([51.5073219, -0.1276474])
+print(location1)
+location1.remove([22.34921555, 114.185797810004]) # "ошибка" алгоритма
+#отображение на карте
 import folium
-map = folium.Map(location=loc1, zoom_start = 5)
-for coordinates in [loc1,loc2,loc3,loc4,loc5]:
+map = folium.Map(location=[51.5073219, -0.1276474], zoom_start = 5)
+for coordinates in location1:
     folium.Marker(location=coordinates, icon=folium.Icon(color = 'green')).add_to(map)
-folium.PolyLine([loc2,loc1,loc3,loc4,loc5]).add_to(map)
+folium.PolyLine([location1]).add_to(map)
 map.save("map2.html")
