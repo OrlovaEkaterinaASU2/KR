a9d41ccf4d6a4645eda3e0338be8af480b5934d6
OrlovaEkaterinaASU2 <orlova_ekaterina_97@mail.ru>
2019-12-05 11:39:17 +0500

    Сдали курсовую работу

--- 1.py
+++ 1.py
@@ -2,7 +2,7 @@ import pymorphy2
 import geocoder
 import natasha
 from collections import OrderedDict
-
+# сдали курсовую работу
 from folium import Popup
 
 morph = pymorphy2.MorphAnalyzer()
@@ -57,9 +57,9 @@ for key in phileasfogg.keys():
     print(phileasfogg[key])
 location1.insert(0,[51.5073219, -0.1276474]) # координаты лондона
 location1.append([51.5073219, -0.1276474])
-#  print(location1)
-location1.remove([22.34921555, 114.185797810004]) # "ошибка" алгоритма'''
-location12=[]  # убрать повторения для корректной нумерации
+print(location1)
+location1.remove([22.3413492, 114.1761659]) # "ошибка" алгоритма'''
+location12 = []  # убрать повторения для корректной нумерации
 for i in location1:
     if i not in location12:
         location12.append(i)
@@ -70,7 +70,7 @@ import folium
 i=0
 map1 = folium.Map(location=[51.5073219, -0.1276474], popup = str(i), zoom_start = 5)
 for coordinates in location12:
-    if i==10:
+    if i==len(location12):
         i=1
     folium.CircleMarker(location=coordinates, popup=Popup(str(i), parse_html=True, show=True),  tooltip=str(i), fill_color="green", color="gray", fill_opacity = 0.9).add_to(map1)
     i=i+1
